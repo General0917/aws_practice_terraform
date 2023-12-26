ba4999ca5d7a8fd202099071c3aa977918eb2761
General0917 <ht13a037@oecu.jp>
2023-12-27 04:34:06 +0900

    chapter13の学習途中

--- chapter13/README.md
+++ chapter13/README.md
@@ -223,5 +223,23 @@ ElastiCacheレプリケーショングループをリスト13.8のように実�
 
 リスト13.8: ElastiCacheレプリケーショングループの定義
 ```
-
+resource "aws_elasticache_replication_group" "example" {
+  replication_group_id = "example"
+  description = "Cluster Disabled"
+  engine = "redis"
+  engine_version = "5.0.4"
+  num_cache_clusters = 3
+  node_type = "cache.m3.medium"
+  snapshot_window = "09:10-10:10"
+  snapshot_retention_limit = 7
+  maintenance_window = "mon:10:40-mon:11:40"
+  automatic_failover_enabled = true
+  port = 6379
+  apply_immediately = false
+  security_group_ids = [module.redis_sg.security_group_id]
+  parameter_group_name = aws_elasticache_parameter_group.example.name
+  subnet_group_name = aws_elasticache_subnet_group.example.name
+}
 ```
+
+#### 識別子
--- chapter13/main.tf
+++ chapter13/main.tf
@@ -107,3 +107,28 @@ resource "aws_elasticache_subnet_group" "example" {
 }
 
 # ElastiCacheレプリケーショングループの定義
+resource "aws_elasticache_replication_group" "example" {
+  replication_group_id = "example"
+  description = "Cluster Disabled"
+  engine = "redis"
+  engine_version = "5.0.4"
+  num_cache_clusters = 3
+  node_type = "cache.m3.medium"
+  snapshot_window = "09:10-10:10"
+  snapshot_retention_limit = 7
+  maintenance_window = "mon:10:40-mon:11:40"
+  automatic_failover_enabled = true
+  port = 6379
+  apply_immediately = false
+  security_group_ids = [module.redis_sg.security_group_id]
+  parameter_group_name = aws_elasticache_parameter_group.example.name
+  subnet_group_name = aws_elasticache_subnet_group.example.name
+}
+
+module "redis_sg" {
+  source = "./security_group"
+  name = "redis-sg"
+  vpc_id = module.vpc.vpc_id
+  port = 6379
+  cidr_blocks = [module.vpc.aws_vpc_example_cidr_block]
+}
